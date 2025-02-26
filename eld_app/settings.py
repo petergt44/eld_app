33dc554415af874117be36ced7702ebaf217dd2b
petergt44 <petergt44@gmail.com>
2025-02-26 16:42:33 +0300

    Configure Django settings

--- eld_app/settings.py
+++ eld_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0u%le!k+@(rhserpa6ed^-ls)=nft(+at!#45-q=fyms-9t1&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = 'secret-key'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
